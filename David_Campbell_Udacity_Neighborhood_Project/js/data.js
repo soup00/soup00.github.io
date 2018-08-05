fddd1136e3f328d06a5bf88574dedadb022baf13
soup00 <davidjaecampbell@gmail.com>
2018-08-05 15:57:19 -0600

    Add files via upload

---
+++ David_Campbell_Udacity_Neighborhood_Project/js/data.js
@@ -0,0 +1,52 @@
+var locations = [
+    {
+        title: 'Dae Gee Korean BBQ',
+        lat: 39.8333825,
+        lng:  -105.052416
+    },
+    {
+        title: 'Seoul Korean BBQ',
+        lat: 39.6788,
+        lng: -104.93507
+    },
+    {
+        title: 'Mr. Kim Korean BBQ',
+        lat: 39.697553,
+        lng: -104.916453
+    },
+    {
+        title: 'Olleh Korean Food',
+        lat: 39.668481,
+        lng: -104.931881
+    },
+    {
+        title: 'Soban Restaurant',
+        lat: 39.6754915,
+        lng: -104.9166654
+    },
+    {
+        title: 'Funny Plus',
+        lat: 39.6659032,
+        lng: -104.9323503
+    },
+    {
+        title: 'Bop & Gogi',
+        lat: 39.5936043,
+        lng: -104.9357514
+    },
+    {
+        title: 'Tofu House 1962',
+        lat: 39.6743025,
+        lng: -104.9376405
+    },
+    {
+        title: 'Silla Restaurant',
+        lat: 39.6615147,
+        lng: -104.9180911
+    },
+    {
+        title: 'Karma Asian',
+        lat: 39.7160891,
+        lng: -105.0572037
+    }
+]
